6545529cf0d583eb3928413e763b667da5e9aa83
Manav <mtech707070@gmail.com>
2023-05-20 22:03:52 +0530

    new question added

---
+++ TwoDArray/basic.java
@@ -0,0 +1,23 @@
+package TwoDArray;
+
+import java.util.Scanner;
+
+public class basic {
+    public static void main(String[] args) {
+        int matrix[][]=new int[3][3];
+        int n=matrix.length,m=matrix.length;
+        Scanner sc=new Scanner(System.in);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                 matrix[i][j]=sc.nextInt();
+            }
+           
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                System.out.print(matrix[i][j]+ " ");
+            }
+            System.out.println();
+        }
+    }
+}
