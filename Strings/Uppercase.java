ed002b8af1251138d770d5f66608f4828c853cbd
Manav <mtech707070@gmail.com>
2023-05-24 10:01:40 +0530

    new code added

---
+++ Strings/Uppercase.java
@@ -0,0 +1,12 @@
+package Strings;
+
+public class Uppercase {
+    public static String toUpperCase(String str){
+        StringBuilder sb=new StringBuilder("");
+        char ch=Character.toUpperCase(str.charAt(0));
+        sb.append(ch)
+    }
+    public static void main(String[] args) {
+        
+    }
+}
