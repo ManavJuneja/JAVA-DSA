5ec046428fd7a781098a4e4753651d257adf6ec4
Manav <97352650+ManavJuneja@users.noreply.github.com>
2023-07-27 19:38:26 +0530

    new xode

---
+++ Stacks/maxAreaHistogram.java
@@ -0,0 +1,60 @@
+package Stacks;
+
+import java.util.Stack;
+
+public class maxAreaHistogram {
+    public static void maxArea(int arr[]){
+        int maxArea=0;
+        int nsr[]= new int[arr.length];
+        int nsl[]= new int[arr.length];
+
+        //Next Smaller Right
+        Stack<Integer> s=new Stack<>();
+
+        for(int i=arr.length-1;i>=0;i--){
+
+            while(!s.isEmpty() && arr[s.peek()]>=arr[i]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                //-1
+                nsr[i]=arr.length;
+            }
+            else{
+                //top
+                nsr[i]=s.peek();
+            }
+            s.push(i);
+        }
+        //Next Smaller Left
+        s=new Stack<>();
+        for(int i=0;i<arr.length;i++){
+            while(!s.isEmpty() && arr[s.peek()]>=arr[i]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                //-1
+                nsl[i]=-1;
+            }
+            else{
+                //top
+                nsl[i]=s.peek();
+            }
+            s.push(i);
+        }
+
+        //Current Area width = j-i-1 =nsr[i]-nsl[i]-1
+        for(int i=0;i<arr.length;i++){
+            int height=arr[i];
+            int width=nsr[i]-nsl[i]-1;
+            int currArea=height*width;
+            maxArea=Math.max(maxArea, currArea);
+        }
+        System.out.println("Maximum area in histogram is "+maxArea);
+
+    }
+    public static void main(String[] args) {
+        int arr[]={2,1,5,6,2,3}; //height in histogram
+        maxArea(arr);
+    }
+}
