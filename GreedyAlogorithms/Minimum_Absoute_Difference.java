d3f013576cc736bee3a5bd05d9f746e8f71937a5
Manav <mtech707070@gmail.com>
2023-08-11 08:14:19 +0530

    new code

---
+++ GreedyAlogorithms/Minimum_Absoute_Difference.java
@@ -0,0 +1,17 @@
+package GreedyAlogorithms;
+import java.util.*;
+
+public class Minimum_Absoute_Difference {
+    public static void main(String[] args) {
+        int A[]={1,2,3};
+        int B[]={2,1,3};
+        
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int minDiff=0;
+        for(int i=0;i<A.length;i++){
+            minDiff+=Math.abs(A[i]-B[i]);
+        }
+        System.out.println("Minimum absoulte pair difference "+minDiff);
+    }
+}
